701306913f9df5abc1b29f5397d3d9032af8cf0b
thorgrim88@gmail.com <thorgrim88@gmail.com>
2021-02-05 09:56:40 -0800

    add good bad fail Mars Excess test

--- __test__/mars.test.js
+++ __test__/mars.test.js
@@ -6,4 +6,14 @@ describe('MarsAge', () => {
     let marsAge = parseInt(age *= marsTime);
     expect(marsAge).toEqual(37)
   })
+  test('Intake age, if over 72 years return number of excess', () => {
+    let age = 50;
+    let marsTime = 1.88;
+    let marsAge = parseInt(age *= marsTime);
+    let excessMarsAge = 0
+    if (marsAge > 72) {
+      excessMarsAge = marsAge - 72
+    }
+    expect(excessMarsAge).toEqual()
+  })
 })
