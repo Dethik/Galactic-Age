8c87c28a37c575a7e42d17cce6633a776dfa3e68
thorgrim88@gmail.com <thorgrim88@gmail.com>
2021-02-05 09:49:04 -0800

    move mars test to proper file, add successful age test

--- __test__/jupiter.test.js
+++
@@ -1,9 +0,0 @@
-describe('MarsAge', () => {
-
-  test('Intake age from user and give them age if they were a Martian', () => {
-    let age = 20;
-    let marsTime = 1.88;
-    let marsAge = parseInt(age *= marsTime);
-    expect(marsAge).toEqual(37)
-  })
-})
