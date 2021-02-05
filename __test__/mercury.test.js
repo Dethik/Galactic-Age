b39fb6fcae64dac424675d548aab5e9a9aababc2
thorgrim88@gmail.com <thorgrim88@gmail.com>
2021-02-05 08:35:23 -0800

    add good bad fail for mercury

--- __test__/mercury.test.js
+++ __test__/mercury.test.js
@@ -1,11 +1,11 @@
 const { TestScheduler } = require("jest")
 
 describe('mercAge', () => {
-  
+
   test('Intake age from user and give them age if they were a Mercurite', () => {
     let age = 20
     let mercTime = .24
     let mercAge = age *= mercTime
-    expect(age * mercTime).toEqual(mercAge)
+    expect(mercAge).toEqual()
   })
 })
