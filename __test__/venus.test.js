35be0a254898b99d173a9eac06784334103f39e9
thorgrim88@gmail.com <thorgrim88@gmail.com>
2021-02-05 09:14:55 -0800

    add good bad fail for Venus test

---
+++ __test__/venus.test.js
@@ -0,0 +1,9 @@
+describe('VenusAge', () => {
+
+  test('Intake age from user and give them age if they were a Venusite', () => {
+    let age = 20;
+    let venusTime = .62;
+    let venusAge = age *= venusTime;
+    expect(venusAge).toEqual()
+  })
+})
---
+++ src/js/agecalc.js
@@ -0,0 +1,7 @@
+let age = userInput
+let mercTime = .24
+let mercAge = age *= mercTime
+let excessMercAge = 0
+if (mercAge > 72) {
+  excessMercAge = mercAge - 72
+}
