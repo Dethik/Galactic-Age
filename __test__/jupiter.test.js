7ad8792d23f2b37cdb63446ebddb64fb58cbab4f
thorgrim88@gmail.com <thorgrim88@gmail.com>
2021-02-05 10:39:36 -0800

    add good bad fail for Excess Jupiter Age Test

--- __test__/jupiter.test.js
+++ __test__/jupiter.test.js
@@ -6,4 +6,14 @@ describe('JupiterAge', () => {
     let jupiterAge = parseInt(age *= jupiterTime);
     expect(jupiterAge).toEqual(237)
   })
+  test('Intake age, if over 72 years return number of excess', () => {
+    let age = 20;
+    let jupiterTime = 11.86;
+    let jupiterAge = parseInt(age *= jupiterTime);
+    let excessJupiterAge = 0
+    if (jupiterAge > 72) {
+      excessJupiterAge = jupiterAge - 72
+    }
+    expect(excessJupiterAge).toEqual()
+  })
 })
