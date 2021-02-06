7775df6a955832420c3c76ae66f05ce720b9f2e5
thorgrim88@gmail.com <thorgrim88@gmail.com>
2021-02-05 16:47:42 -0800

    scrapped html, add console logs to display results and finalize

--- src/js/agecalc.js
+++ src/js/agecalc.js
@@ -1,4 +1,4 @@
-class Planet {
+export default class Planet {
   constructor(age) {
     this.time = 0;
     this.age = age;
@@ -10,69 +10,60 @@ class Planet {
 Planet.prototype.mercuryTime = function() {
   this.time = .24;
   this.age = parseInt(this.age *= this.time);
-  this.lifeExp = parseInt(72 * .24)
+  this.lifeExp = parseInt(72 * .24);
   if (this.lifeExp > this.age) {
-    this.lifeLeft = parseInt(this.lifeExp - this.age)
+    this.lifeLeft = parseInt(this.lifeExp - this.age);
   } else {
-    this.lifeLeft = "'You've outlived expectations, keep it up!'"
+    this.lifeLeft = "'You've outlived expectations, keep it up!'";
   }
   if (this.age > this.lifeExp) {
-    this.excessAge = this.age - this.lifeExp
+    this.excessAge = this.age - this.lifeExp;
   } else {
-    this.excessAge = 0
+    this.excessAge = 0;
   }
 };
 Planet.prototype.venusTime = function() {
   this.time = .62;
   this.age = parseInt(this.age *= this.time);
-  this.lifeExp = parseInt(72 * .62)
+  this.lifeExp = parseInt(72 * .62);
   if (this.lifeExp > this.age) {
-    this.lifeLeft = parseInt(this.lifeExp - this.age)
+    this.lifeLeft = parseInt(this.lifeExp - this.age);
   } else {
-    this.lifeLeft = "'You've outlived expectations, keep it up!'"
+    this.lifeLeft = "'You've outlived expectations, keep it up!'";
   }
   if (this.age > this.lifeExp) {
-    this.excessAge = this.age - this.lifeExp
+    this.excessAge = this.age - this.lifeExp;
   } else {
-    this.excessAge = 0
+    this.excessAge = 0;
   }
 };
 Planet.prototype.marsTime = function() {
   this.time = 1.88;
   this.age = parseInt(this.age *= this.time);
-  this.lifeExp = parseInt(72 * 1.88)
+  this.lifeExp = parseInt(72 * 1.88);
   if (this.lifeExp > this.age) {
-    this.lifeLeft = parseInt(this.lifeExp - this.age)
+    this.lifeLeft = parseInt(this.lifeExp - this.age);
   } else {
-    this.lifeLeft = "'You've outlived expectations, keep it up!'"
+    this.lifeLeft = "'You've outlived expectations, keep it up!'";
   }
   if (this.age > this.lifeExp) {
-    this.excessAge = this.age - this.lifeExp
+    this.excessAge = this.age - this.lifeExp;
   } else {
-    this.excessAge = 0
+    this.excessAge = 0;
   }
 };
 Planet.prototype.jupiterTime = function() {
   this.time = 11.86;
   this.age = parseInt(this.age *= this.time);
-  this.lifeExp = parseInt(72 * 11.86)
+  this.lifeExp = parseInt(72 * 11.86);
   if (this.lifeExp > this.age) {
-    this.lifeLeft = parseInt(this.lifeExp - this.age)
+    this.lifeLeft = parseInt(this.lifeExp - this.age);
   } else {
-    this.lifeLeft = "'You've outlived expectations, keep it up!'"
+    this.lifeLeft = "'You've outlived expectations, keep it up!'";
   }
   if (this.age > this.lifeExp) {
-    this.excessAge = this.age - this.lifeExp
+    this.excessAge = this.age - this.lifeExp;
   } else {
-    this.excessAge = 0
+    this.excessAge = 0;
   }
 };
-const person = new Planet(27)
-
-// Comment and Uncomment the different functions to change results
-
-person.mercuryTime()
-// person.venusTime()
-// person.marsTime()
-// person.jupiterTime()
-console.log('The time scales' + person.time + 'compared to Earth. If your age is' + person.age + 'then your life expectancy is' + person.lifeExp + 'with' + person.lifeLeft + 'years left to live and are' + person.excessAge + 'past this age!')
--- src/main.js
+++ src/main.js
@@ -1,9 +1,20 @@
+import Planet from './js/agecalc.js';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import $ from 'jquery';
 
 $(document).ready(function() {
-  event.preventDefault();
-  console.log()
+  let person = new Planet(27);
+  let person1 = new Planet(50);
+  let person2 = new Planet(67);
+  let person3 = new Planet(80);
+  person.mercuryTime();
+  person1.venusTime();
+  person2.marsTime();
+  person3.jupiterTime();
+  console.log('The time scales ' + person.time + ' compared to Earth. If your age is ' + person.age + ' then your life expectancy is ' + person.lifeExp + ' you are at ' + person.lifeLeft + ' You are currently ' + person.excessAge + ' years past this age!');
+  console.log('The time scales ' + person1.time + ' compared to Earth. If your age is ' + person1.age + ' then your life expectancy is ' + person1.lifeExp + ' you are at ' + person1.lifeLeft + ' You are currently ' + person1.excessAge + ' years past this age!');
+  console.log('The time scales ' + person2.time + ' compared to Earth. If your age is ' + person2.age + ' then your life expectancy is ' + person2.lifeExp + ' you are at ' + person2.lifeLeft + ' You are currently ' + person2.excessAge + ' years past this age!');
+  console.log('The time scales ' + person3.time + ' compared to Earth. If your age is ' + person3.age + ' then your life expectancy is ' + person3.lifeExp + ' you are at ' + person3.lifeLeft + ' You are currently ' + person3.excessAge + ' years past this age!');
 });
