c8a5b112a259ecd9dbe3b86e85cbdbc0a12e7630
thorgrim88@gmail.com <thorgrim88@gmail.com>
2021-02-05 16:21:15 -0800

    add temp literals to console log

--- src/main.js
+++ src/main.js
@@ -2,11 +2,8 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import $ from 'jquery';
-// import Planet from "./js/agecalc.js"
 
 $(document).ready(function() {
   event.preventDefault();
-  let creature = new Planet(26)
-  creature.mercuryTime
-  console.log(creature)
+  console.log()
 });
