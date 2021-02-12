cd2ca08f1b5321e370c380ba8d3fd0e572133782
thorgrim88@gmail.com <thorgrim88@gmail.com>
2021-02-11 21:48:10 -0800

    finalize changes to outcome text

--- src/main.js
+++ src/main.js
@@ -15,9 +15,9 @@ $(document).ready(function() {
     person1.venusTime();
     person2.marsTime();
     person3.jupiterTime();
-    $("#outputMercury").text(`The time scales ` + person.time + ` compared to Earth. If your age is ` + person.age + ` then your life expectancy is ` + person.lifeExp + ` you are at ` + person.lifeLeft + ` You are currently ` + person.excessAge + ` years past this age!`);
-    $("#outputVenus").text(`The time scales ` + person1.time + ` compared to Earth. If your age is ` + person1.age + ` then your life expectancy is ` + person1.lifeExp + ` you are at ` + person1.lifeLeft + ` You are currently ` + person1.excessAge + ` years past this age!`);
-    $("#outputMars").text(`The time scales ` + person2.time + ` compared to Earth. If your age is ` + person2.age + ` then your life expectancy is ` + person2.lifeExp + ` you are at ` + person2.lifeLeft + ` You are currently ` + person2.excessAge + ` years past this age!`);
-    $("#outputJupiter").text(`The time scales ` + person3.time + ` compared to Earth. If your age is ` + person3.age + ` then your life expectancy is ` + person3.lifeExp + ` you are at ` + person3.lifeLeft + ` You are currently ` + person3.excessAge + ` years past this age!`);
+    $("#outputMercury").text(`The time scales ` + person.time + ` compared to Earth. If your age is ` + person.age + ` then your life expectancy is ` + person.lifeExp + ` you are at ` + person.lifeLeft + ` and currently ` + person.excessAge + ` years past this age!`);
+    $("#outputVenus").text(`The time scales ` + person1.time + ` compared to Earth. If your age is ` + person1.age + ` then your life expectancy is ` + person1.lifeExp + ` you are at ` + person1.lifeLeft + ` and currently ` + person1.excessAge + ` years past this age!`);
+    $("#outputMars").text(`The time scales ` + person2.time + ` compared to Earth. If your age is ` + person2.age + ` then your life expectancy is ` + person2.lifeExp + ` you are at ` + person2.lifeLeft + ` and currently ` + person2.excessAge + ` years past this age!`);
+    $("#outputJupiter").text(`The time scales ` + person3.time + ` compared to Earth. If your age is ` + person3.age + ` then your life expectancy is ` + person3.lifeExp + ` you are at ` + person3.lifeLeft + ` and currently ` + person3.excessAge + ` years past this age!`);
   });
 });
